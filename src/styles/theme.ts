aff227e9f14ce0549c01bc264326096cc7416d15
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-03 08:28:35 -0400

    feat: add colors & styles chakra theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    highlight: "#FFBA08",
+    black: "#000000",
+    textDark: "#47585B",
+    infoDark: "#999999",
+    white: "#FFFFFF",
+    textLight: "#F5F8FA",
+    infoLight: "#DADADA",
+  },
+
+  styles: {
+    global: {
+      body: {
+        bgColor: "textLight",
+      },
+    },
+  },
+});
