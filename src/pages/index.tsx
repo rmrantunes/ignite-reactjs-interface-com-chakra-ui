0e630abf2c89aa90eba6889cc8d519e48306eed3
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-03 13:35:09 -0400

    feat: Home page finished but carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { HomeHero } from "components/HomeHero";
+import { Container } from "components/Container";
+import { DestinationTypesList } from "components/DestinationTypesList";
+import { Text } from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main>
+      <HomeHero />
+      <Container>
+        <DestinationTypesList />
+      </Container>
+      <Container py={{ base: "5", md: "12" }}>
+        <Text
+          color="textDark"
+          textAlign="center"
+          fontSize={{ base: "lg", md: "4xl" }}
+          fontWeight="medium"
+        >
+          Vamos nessa? <br /> Então escolha seu continente
+        </Text>
+      </Container>
+      <Container enablePadding={false}>Slide</Container>
+    </main>
+  );
 }
