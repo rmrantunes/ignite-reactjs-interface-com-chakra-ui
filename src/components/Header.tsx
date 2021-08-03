70dc259c0415a44cb677d2d7d8c5fca86f9263bb
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-03 14:07:29 -0400

    feat: add go-to-home link to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
-import { Center, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
+import {
+  Box,
+  Center,
+  SimpleGrid,
+  useBreakpointValue,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+
+import { Container } from "./Container";
 
 import logoImg from "../../public/logo.svg";
+import arrowLeftImg from "../../public/arrow-left.svg";
 
 export default function Header() {
   const logoWidth = useBreakpointValue({
@@ -9,9 +20,30 @@ export default function Header() {
     lg: 185,
   });
 
+  const router = useRouter();
+  const isNotHomePage = router.asPath !== "/";
+
   return (
-    <Center p={{ base: "4", lg: "7" }}>
-      <Image src={logoImg} alt="worldtrip" width={logoWidth} />
-    </Center>
+    <Box as="header" bgColor="textLight">
+      <Container>
+        <SimpleGrid templateColumns="3rem 1fr 3rem">
+          {isNotHomePage && (
+            <Link href="/" passHref>
+              <ChakraLink
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                aria-label="Go back to home"
+              >
+                <Image src={arrowLeftImg} alt="" />
+              </ChakraLink>
+            </Link>
+          )}
+          <Center py={{ base: "4", lg: "7" }} gridColumn="2">
+            <Image src={logoImg} alt="worldtrip" width={logoWidth} />
+          </Center>
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 }
