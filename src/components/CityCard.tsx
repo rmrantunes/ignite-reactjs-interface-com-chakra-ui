1ea66fb50313e71e367b216da3bdbf23ec592dd2
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-05 07:35:01 -0400

    feat: add rounded & border to CityCard

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -11,13 +11,21 @@ type CityCardProps = {
 export const CityCard = (props: CityCardProps) => {
   return (
     <SimpleGrid templateRows="11rem auto">
-      <Cover bgImage={props.image} aria-label={props.city} />
+      <Cover
+        bgImage={props.image}
+        aria-label={props.city}
+        borderTopRadius="4"
+      />
       <Flex
         px="6"
         py="4"
         bgColor="white"
         alignItems="center"
         justify="space-between"
+        borderBottomRadius="4"
+        borderWidth="1px"
+        borderColor="highlight"
+        borderTopColor="transparent"
       >
         <VStack alignItems="start" justify="space-between">
           <Text as="strong">{props.city}</Text>
