877beefc45ec15d70231c1431cdc15833508cca2
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-05 15:11:28 -0400

    feat: add bgBlendMode if darken prop to Cover

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -2,14 +2,23 @@ import { Box, BoxProps } from "@chakra-ui/react";
 
 type RequiredCoverProps = Required<Pick<BoxProps, "aria-label" | "bgImage">>;
 
-type CoverProps = BoxProps & RequiredCoverProps;
+type CoverProps = BoxProps &
+  RequiredCoverProps & {
+    darken?: boolean;
+  };
 
 export const Cover = (props: CoverProps) => {
+  const darken = props.darken && {
+    bgBlendMode: "darken",
+  };
+
   return (
     <Box
       bgSize="cover"
       bgPosition="center center"
       bgRepeat="no-repeat"
+      bgColor="rgba(0,0,0,0.3)"
+      {...darken}
       {...props}
     />
   );
